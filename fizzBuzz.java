062523d3aa9920ea862910d64757daedf787710f
BryantCR <bryancvargas20@gmail.com>
2021-10-18 12:58:01 -0600

    ok

---
+++ fizzBuzz.java
@@ -0,0 +1,24 @@
+
+
+
+public class FizzBuzz {
+    public String fizzBuzz(int number) {
+        // fizzbuzz logic here - returns "Fizz", "Buzz", "FizzBuzz"
+        // or a String cast of the number itself. 
+        // Hint: You can use a String method to cast the int to a String.
+    }
+    
+    // NINJA BONUS:
+    // Create an overloaded method for fizzBuzz
+    public String fizzBuzz(
+        // Parameters
+        int number,  
+        String multOf3Word, 
+        String multOf5Word,
+        String multOf15Word
+        ) {
+        
+        // Implement the same logic, but replace "Fizz", "Buzz" and 
+        // "Fizzbuzz" with the given words
+    }
+}
